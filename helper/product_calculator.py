52dbe94ca8efa3cb14b6b9720f092a6a82dced47
ShoY634 <shota20000228@gmail.com>
2023-03-12 16:33:24 +0900

    quantity取得部分微修正

--- helper/product_calculator.py
+++ helper/product_calculator.py
@@ -5,15 +5,16 @@ from fixtures.product_data import pc
 class Calculator:
     purchase_list = [{"product": pc, "quantity": 1}]
     
+    
     @classmethod
     def purchase_product(cls, purchase_list):
         # product_purchased = purchase_list[0]["product"]
         for purchase in purchase_list:
             product_purchased = purchase["product"]
-        product_purchased_quantity = purchase_list[0]["quantity"]
+            product_purchased_quantity = purchase["quantity"]
         product_purchased_price = int(product_purchased_quantity * product_purchased.price * 1.1)
         
-
+        
         print(f"商品「{product_purchased.name}」を{str(product_purchased_quantity)}個購入しました。")
         
         pc.stock = pc.stock - product_purchased_quantity
