222a9edbf118439027979db9f2cfff70b0b10daf
ShoY634 <shota20000228@gmail.com>
2023-03-13 01:15:49 +0900

    add_stockメソッドを追加。purchase_productを修正。その他可読性上げる微修正。

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fixtures.product_data import pc
+from fixtures.product_data import pc, mouse
 from helper.product_calculator import Calculator
 
 
-a = Calculator.purchase_product(purchase_list = [{"product": pc, "quantity": 1}])
+a = Calculator.purchase_product(purchase_list = [{"product": pc, "quantity": 1}, {"product": mouse, "quantity": 2}])
+b = Calculator.add_stock(stock_add_list = [{'product': pc, 'quantity_add': 2}, {"product": mouse, "quantity_add": 1}])
